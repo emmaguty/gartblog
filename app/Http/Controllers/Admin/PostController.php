99408dcb4855855e0e7d3bc6b00f4baf7a62d2e0
emmanuelguty <manuel15901@gmail.com>
2022-12-07 18:49:40 -0600

    Ya se puede eliminar los posts

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -133,6 +133,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route('admin.posts.index')->with('info', 'El post se eliminó con éxito');
     }
 }
